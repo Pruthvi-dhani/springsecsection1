ff4db85e499e087512651e68d5a4b4d802c457e8
Pruthvi-dhani <87530402+Pruthvi-dhani@users.noreply.github.com>
2026-01-02 17:45:17 +0000

    login settings and welcome controller added

--- src/main/java/com/udemycourse/springsecsection1/EasyBankBackendApplication.java
+++ src/main/java/com/udemycourse/springsecsection1/EasyBankBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Springsecsection1Application {
+public class EasyBankBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Springsecsection1Application.class, args);
+		SpringApplication.run(EasyBankBackendApplication.class, args);
 	}
 
 }
