e892eda4629ecc2016ad8ac6f61f7923ea4faea3
Pruthvi-dhani <87530402+Pruthvi-dhani@users.noreply.github.com>
2026-01-31 14:29:22 +0000

    use of bcrypt password encoder

--- src/main/java/com/udemycourse/springsecsection1/config/ProjectSecurityConfig.java
+++ src/main/java/com/udemycourse/springsecsection1/config/ProjectSecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -26,8 +28,14 @@ public class ProjectSecurityConfig {
 
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails user = User.withUsername("user").password("12345").authorities("read").build();
-        UserDetails admin = User.withUsername("admin").password("{noop}54321").authorities("admin").build();
+        UserDetails user = User.withUsername("user").password("{bcrypt}$2a$12$svkCQYDC8sdtJaCuoiSMXOJfWrgt1tmwnfJ0yg5vk2/eGYcrpJFGa").authorities("read").build();
+        UserDetails admin = User.withUsername("admin").password("{bcrypt}$2a$12$k9N6.VShRRYAAuiia9/Rieus8QwL0gNWlZbLzCf8x2ma/zWoXbRdO").authorities("admin").build();
         return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoderService() {
+        // uses bcrypt password encoder by default
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
